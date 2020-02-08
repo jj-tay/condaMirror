2487112ac820762e2bfa668428eb7e289b5ae473
Tay Jun Jie <jjat1987@gmail.com>
2020-02-08 21:57:41 +0800

    Use loops to condense code

--- download_anaconda.sh
+++ download_anaconda.sh
@@ -7,59 +7,51 @@ source $HOME/anaconda3/etc/profile.d/conda.sh
 RESULT=$(conda env list | grep -c conda-mirror)
 if [ $RESULT -eq 0 ]
 then
-	conda create -y -n conda-mirror -c conda-forge conda-mirror
+	conda env create -f environment.yml
 	conda activate conda-mirror
 else
 	conda activate conda-mirror
 fi
 
-# Create local dir for mirror if required
-MIRROR_DIR=$PWD
+# Download main, free, r channels for win-64, linux-64, noarch
+for CHANNEL in main free r
+do
+  for PLATFORM in win-64 linux-64 noarch
+  do
+    conda-mirror \
+	  --upstream-channel https://repo.continuum.io/pkgs/$CHANNEL \
+	  --target-directory $CHANNEL \
+	  --platform $PLATFORM \
+	  -vv
+  done
+done
 
-# Download main channel for win-64
-conda-mirror --upstream-channel https://repo.continuum.io/pkgs/main --target-directory main --temp-directory "$MIRROR_DIR" --platform win-64 -vv
-
-# Download main channel for linux-64
-conda-mirror --upstream-channel https://repo.continuum.io/pkgs/main --target-directory main --temp-directory "$MIRROR_DIR" --platform linux-64 -vv
-
-# Download main channel for noarch
-conda-mirror --upstream-channel https://repo.continuum.io/pkgs/main --target-directory main --temp-directory "$MIRROR_DIR" --platform noarch -vv
-
-# Download free channel for win-64
-conda-mirror --upstream-channel https://repo.continuum.io/pkgs/free --target-directory free --temp-directory "$MIRROR_DIR" --platform win-64 -vv
-
-# Download free channel for linux-64
-conda-mirror --upstream-channel https://repo.continuum.io/pkgs/free --target-directory free --temp-directory "$MIRROR_DIR" --platform linux-64 -vv
-
-# Download free channel for noarch
-conda-mirror --upstream-channel https://repo.continuum.io/pkgs/free --target-directory free --temp-directory "$MIRROR_DIR" --platform noarch -vv
-
-# Download r channel for win-64
-conda-mirror --upstream-channel https://repo.continuum.io/pkgs/r --target-directory r --temp-directory "$MIRROR_DIR" --platform win-64 -vv
-
-# Download r channel for linux-64
-conda-mirror --upstream-channel https://repo.continuum.io/pkgs/r --target-directory r --temp-directory "$MIRROR_DIR" --platform linux-64 -vv
-
-# Download r channel for noarch
-conda-mirror --upstream-channel https://repo.continuum.io/pkgs/r --target-directory r --temp-directory "$MIRROR_DIR" --platform noarch -vv
+# Download conda-forge channel for win-64, linux-64, noarch
+for PLATFORM in win-64 linux-64 noarch
+do
+    conda-mirror \
+	  --upstream-channel https://conda.anaconda.org/conda-forge \
+	  --target-directory conda-forge \
+	  --platform $PLATFORM \
+	  -vv
+done
 
 # Download msys2 channel for win-64
-conda-mirror --upstream-channel https://repo.continuum.io/pkgs/msys2 --target-directory msys2 --temp-directory "$MIRROR_DIR" --platform win-64 -vv
+conda-mirror \
+  --upstream-channel https://repo.continuum.io/pkgs/msys2 \
+  --target-directory msys2 \
+  --platform win-64 \
+  -vv
 
-# Download esri channel for win-64
-conda-mirror --upstream-channel https://conda.anaconda.org/esri/ --target-directory esri --temp-directory "$MIRROR_DIR" --platform win-64 -vv
-
-# Download esri channel for noarch
-conda-mirror --upstream-channel https://conda.anaconda.org/esri/ --target-directory esri --temp-directory "$MIRROR_DIR" --platform noarch -vv
-
-# Download conda-forge channel for win-64
-conda-mirror --upstream-channel https://conda.anaconda.org/conda-forge --target-directory conda-forge --temp-directory "$MIRROR_DIR" --platform win-64 -vv
-
-# Download conda-forge channel for linux-64
-conda-mirror --upstream-channel https://conda.anaconda.org/conda-forge --target-directory conda-forge --temp-directory "$MIRROR_DIR" --platform linux-64 -vv
-
-# Download conda-forge channel for noarch
-conda-mirror --upstream-channel https://conda.anaconda.org/conda-forge --target-directory conda-forge --temp-directory "$MIRROR_DIR" --platform noarch -vv
+# Download esri channel for win-64, noarch
+for PLATFORM in win-64 noarch
+do
+  conda-mirror \
+  --upstream-channel https://conda.anaconda.org/esri/ \
+  --target-directory esri \
+  --platform $PLATFORM \
+  -vv
+done
 
 # Exit conda-mirror environment and return to previous directory
 conda deactivate
