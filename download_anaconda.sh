cb01472b488dd6dd47f21ff1e26d1e5e7224107d
Tay Jun Jie <jjat1987@gmail.com>
2023-08-19 22:16:27 +0800

    Update bash script

--- download_anaconda.sh
+++ download_anaconda.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 set -e
 
-# Record start time for use to detect new files later
-START_DATETIME=$(date +"%F %T")
-
-# Setup conda
-source $HOME/miniconda3/etc/profile.d/conda.sh
-
-# Create conda-miror environment if required
-RESULT=$(conda env list | grep -c conda-mirror)
-if [ $RESULT -eq 0 ]
-then
-	conda env create -f environment.yml
-	conda activate conda-mirror
-else
-	conda activate conda-mirror
-fi
-
 # Set temp directory
 TEMPDIR=tempdir
 
@@ -26,25 +10,25 @@ do
   for PLATFORM in win-64 linux-64 noarch
   do
     conda-mirror \
-	  --upstream-channel https://repo.continuum.io/pkgs/$CHANNEL \
+      --upstream-channel https://repo.continuum.io/pkgs/$CHANNEL \
       --temp-directory $TEMPDIR \
-	  --target-directory condaMirror/$CHANNEL \
-	  --platform $PLATFORM \
-	  -vv
+	    --target-directory condaMirror/$CHANNEL \
+	    --platform $PLATFORM \
+	    -vv
   done
 done
 
 # Download conda-forge channel for win-64, linux-64, noarch
 for PLATFORM in win-64 linux-64 noarch
 do
-    conda-mirror \
-	  --upstream-channel https://conda.anaconda.org/conda-forge \
-      --temp-directory $TEMPDIR \
-	  --target-directory condaMirror/conda-forge \
-	  --platform $PLATFORM \
-	  --config whitelist_condaforge.yaml \
-      -D \
-	  -vv
+  conda-mirror \
+    --upstream-channel https://conda.anaconda.org/conda-forge \
+    --temp-directory $TEMPDIR \
+    --target-directory condaMirror/conda-forge \
+    --platform $PLATFORM \
+    --config whitelist_condaforge.yaml \
+    -D \
+    -vv
 done
 
 # Download msys2 channel for win-64
@@ -62,12 +46,12 @@ do
   for PLATFORM in win-64 linux-64 noarch
   do
     conda-mirror \
-	  --upstream-channel https://conda.anaconda.org/$CHANNEL \
+	    --upstream-channel https://conda.anaconda.org/$CHANNEL \
       --temp-directory $TEMPDIR \
-	  --target-directory condaMirror/$CHANNEL \
-	  --platform $PLATFORM \
+	    --target-directory condaMirror/$CHANNEL \
+	    --platform $PLATFORM \
       -D \
-	  -vv
+	    -vv
   done
 done
 
@@ -75,13 +59,13 @@ done
 for PLATFORM in win-64 linux-64 noarch
   do
     conda-mirror \
-	  --upstream-channel https://conda.anaconda.org/pytorch \
+	    --upstream-channel https://conda.anaconda.org/pytorch \
       --temp-directory $TEMPDIR \
-	  --target-directory condaMirror/pytorch \
-	  --platform $PLATFORM \
-	  --config whitelist_pytorch.yaml \
+	    --target-directory condaMirror/pytorch \
+	    --platform $PLATFORM \
+	    --config whitelist_pytorch.yaml \
       -D \
-	  -vv
+	    -vv
   done
 
 # Index the channels
@@ -90,9 +74,5 @@ do
   python -m conda_mirror index $CHANNEL
 done
 
-# Exit conda-mirror environment and return to previous directory
-conda deactivate
-if [ $RESULT -eq 0 ]
-then
-	conda remove -y -n conda-mirror --all
-fi
+# Sync to S3
+aws s3 sync condaMirror s3://s3fs-mount-s3-prod/hdbconda --delete --debug --profile hdbba-s3fs
